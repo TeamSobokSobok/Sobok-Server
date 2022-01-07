fbbc9aaa4d67e91914bc3fe391b86c37bafad21d
kanghanhee <kanghanhee59@gmail.com>
2022-01-07 22:53:47 +0900

    [chore] : 초기 세팅

---
+++ functions/constants/responseMessage.js
@@ -0,0 +1,23 @@
+module.exports = {
+  NULL_VALUE: '필요한 값이 없습니다',
+  OUT_OF_VALUE: '파라미터 값이 잘못되었습니다',
+
+  // 회원가입
+  CREATED_USER: '회원 가입 성공',
+  DELETE_USER: '회원 탈퇴 성공',
+  ALREADY_EMAIL: '이미 사용중인 이메일입니다.',
+
+  // 로그인
+  LOGIN_SUCCESS: '로그인 성공',
+  LOGIN_FAIL: '로그인 실패',
+  NO_USER: '존재하지 않는 회원입니다.',
+  MISS_MATCH_PW: '비밀번호가 맞지 않습니다.',
+
+  // 서버 내 오류
+  INTERNAL_SERVER_ERROR: '서버 내 오류',
+
+  // 토큰
+  TOKEN_EXPIRED: '토큰이 만료되었습니다.',
+  TOKEN_INVALID: '토큰이 유효하지 않습니다.',
+  TOKEN_EMPTY: '토큰이 없습니다.',
+};
---
+++ functions/index.js
@@ -0,0 +1,18 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('./sobok-76d0a-firebase-adminsdk-qb2ez-a9bbadc4b6.json');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+let firebase;
+if (admin.apps.length === 0) {
+  firebase = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+} else {
+  firebase = admin.app();
+}
+
+module.exports = {
+  api: require('./api'),
+};
