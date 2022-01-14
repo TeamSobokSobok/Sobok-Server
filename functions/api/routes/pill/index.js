d19ff747a1a5c369c73cf4919d5ba2b294a7d782
Crayon <lsh328328@naver.com>
2022-01-15 00:32:01 +0900

    [update] : 변수명 변경 및 내 약 추가 API 구현

--- functions/api/routes/pill/index.js
+++ functions/api/routes/pill/index.js
@@ -3,7 +3,8 @@ const { checkUser } = require('../../../middlewares/auth');
 const router = express.Router();
 
 router.post('/', checkUser, require('./pillAditionalPOST'));
-router.get('/:receiverId', require('./pillCountGET'));
+router.get('/:userId', checkUser, require('./pillCountGET'));
+router.get('/my/count', checkUser, require('./myPillCountGET'));
 router.post('/:receiverId', checkUser, require('./friendPillAditionalPOST'));
 
 module.exports = router;
