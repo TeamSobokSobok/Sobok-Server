b30717d31ded0633dcb4e1c8d209b8aae41e2bb9
kanghanhee <kanghanhee59@gmail.com>
2022-01-15 23:01:17 +0900

    [feat] : 복용 완료 체크 취소하기 구현

--- functions/api/routes/schedule/index.js
+++ functions/api/routes/schedule/index.js
@@ -8,4 +8,8 @@ router.get('/:memberId', checkUser, require('./scheduleByMemberIdGET'));
 // 약 세부 스케줄 가져오기
 router.get('/:memberId/detail', checkUser, require('./scheduleByMemberIdDetailGET'));
 
+// 약 스케줄 체크하기
+router.put('/check/:scheduleId', checkUser, require('./scheduleCheckByScheduleIdPUT'));
+router.put('/uncheck/:scheduleId', checkUser, require('./scheduleUnCheckByScheduleIdPUT'));
+
 module.exports = router;
