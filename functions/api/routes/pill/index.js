3569ca7086c30c232bbd3ab42604c8e761abc67a
kanghanhee <kanghanhee59@gmail.com>
2022-01-17 20:20:01 +0900

    [fix] : router 경로 수정

--- functions/api/routes/pill/index.js
+++ functions/api/routes/pill/index.js
@@ -2,17 +2,19 @@ const express = require('express');
 const { checkUser } = require('../../../middlewares/auth');
 const router = express.Router();
 
+// 약 정보 조회 관련 router
+router.get('/:userId/count', checkUser, require('./pillByUserIdCountGET'));
+router.get('/my/count', checkUser, require('./myPillCountGET'));
+
 // 약 추가 관련 router
 router.post('/', checkUser, require('./pillAditionalPOST'));
 router.post('/member/:receiverId', checkUser, require('./friendPillAditionalPOST'));
 
-// 약 정보 조회 관련 router
-router.get('/:userId', checkUser, require('./pillCountGET'));
-router.get('/my/count', checkUser, require('./myPillCountGET'));
-
 // 약 정보 수정 관련 router
 router.put('/:pillId', checkUser, require('./pillModifyPUT'));
-router.delete('/:pillId', checkUser, require('./deleteMyPillDELETE'));
 router.put('/stop/:pillId', checkUser, require('./stopPillPUT.js'));
+
+// 약 삭제 관련 router
+router.delete('/:pillId', checkUser, require('./deleteMyPillDELETE'));
 
 module.exports = router;
