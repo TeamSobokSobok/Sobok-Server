3d661d82a67695af18082caf8c0020be2616b4b2
kanghanhee <kanghanhee59@gmail.com>
2022-01-17 20:54:23 +0900

    [fix] : 내 캘린더 조회 라우터 경로 수정

--- functions/api/routes/schedule/index.js
+++ functions/api/routes/schedule/index.js
@@ -3,10 +3,8 @@ const { checkUser } = require('../../../middlewares/auth');
 const router = express.Router();
 
 // 캘린더 가져오기
-router.get('/', checkUser, require('./scheduleGET'));
-router.get('/:memberId', checkUser, require('./scheduleByMemberIdGET'));
-
-// 약 세부 스케줄 가져오기
+router.get('/calendar', checkUser, require('./scheduleGET'));
+router.get('/:memberId/calendar', checkUser, require('./scheduleByMemberIdGET'));
 router.get('/detail', checkUser, require('./scheduleDetailGET'));
 router.get('/:memberId/detail', checkUser, require('./scheduleByMemberIdDetailGET'));
 
