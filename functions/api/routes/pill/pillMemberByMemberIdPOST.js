70c4430882f0b072b92e40e0f0d9c8a702e70e73
kanghanhee <kanghanhee59@gmail.com>
2022-01-21 21:05:48 +0900

    [fix] : uid 불러오기 버그 수정

--- functions/api/routes/pill/pillMemberByMemberIdPOST.js
+++ functions/api/routes/pill/pillMemberByMemberIdPOST.js
@@ -123,7 +123,7 @@ module.exports = async (req, res) => {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
     console.log(error);
 
-    const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${req.user ? `uid:${req.user.id}` : 'req.user 없음'} ${JSON.stringify(error)}`;
+    const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${req.user ? `uid:${req.header.user.id}` : 'req.user 없음'} ${JSON.stringify(error)}`;
     slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
 
     res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
