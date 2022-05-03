df1852da779af0e5638ab6b44c0c2ba1c89b0cde
kanghanhee <kanghanhee59@gmail.com>
2022-05-04 01:10:36 +0900

    chore: new foldering

---
+++ functions/service/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  authService: require('./authService'),
+  noticeService: require('./noticeService'),
+  pillService: require('./pillService'),
+  scheduleService: require('./scheduleService'),
+  stickerService: require('./stickerService'),
+  userService: require('./userService'),
+};
