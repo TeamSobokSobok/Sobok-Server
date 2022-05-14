48aa235175b3d917c098d057c1425ef19effa43d
kanghanhee <kanghanhee59@gmail.com>
2022-05-14 20:59:30 +0900

    #139 update: 경로 다시 설정

--- functions/api/routes/user/index.js
+++ functions/api/routes/user/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const userContoller = require('../../../controller/userContoller');
 const { checkUser } = require('../../../middlewares/auth');
 const router = express.Router();
 
-router.get('/', checkUser, require('./userGET'));
+router.get('/', checkUser, userContoller.getUsername);
 
 // 닉네임 중복검사
-router.post('/name', require('./userNamePOST'));
+router.post('/name', userContoller.checkNickname);
 
 module.exports = router;
