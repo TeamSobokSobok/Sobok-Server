847ca65278ac72dda3d46449f4f7b50dcc6fa798
kanghanhee <kanghanhee59@gmail.com>
2022-05-14 20:53:18 +0900

    #139 update: 회원가입 로직 분리

--- functions/controller/authController.js
+++ functions/controller/authController.js
@@ -3,29 +3,57 @@ const util = require('../../../lib/util');
 const statusCode = require('../../../constants/statusCode');
 const responseMessage = require('../../../constants/responseMessage');
 const db = require('../../../db/db');
+const functions = require('firebase-functions');
+
 const { authService } = require('../service');
 
-module.exports = async (req, res) => {
-  let client;
+module.exports = {
+  authEmail: async (req, res) => {
+    let client;
 
-  try {
-    client = await db.connect(req);
+    try {
+      client = await db.connect(req);
 
-    const { email, password } = req.body;
+      const { email, password } = req.body;
 
-    if (!email || !password) {
-      return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+      if (!email || !password) {
+        return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+      }
+
+      const user = authService.authEmail(email, password);
+
+      res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.LOGIN_SUCCESS, user));
+    } catch (error) {
+      console.log(error);
+      functions.logger.error(`[EMAIL LOGIN ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] email:${email} ${error}`);
+
+      res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+    } finally {
+      client.release();
     }
+  },
+  signUp: async (req, res) => {
+    let client;
 
-    const user = authService.authEmail(email, password);
+    try {
+      client = await db.connect(req);
 
-    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.LOGIN_SUCCESS, user));
-  } catch (error) {
-    console.log(error);
-    functions.logger.error(`[EMAIL LOGIN ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] email:${email} ${error}`);
+      const { email, name, password } = req.body;
 
-    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
-  } finally {
-    client.release();
-  }
+      if (!email || !name || !password) {
+        return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+      }
+
+      const result = authService.signUp(email, name, password);
+
+      res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.CREATED_USER, result));
+    } catch (error) {
+      functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+      console.log(error);
+
+      res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+    } finally {
+      client.release();
+    }
+  },
 };
--- functions/service/authService.js
+++ functions/service/authService.js
@@ -3,6 +3,7 @@ const { firebaseAuth } = require('../config/firebaseClient');
 const { userDB } = require('../db');
 const db = require('../db/db');
 const util = require('../lib/util');
+const admin = require('firebase-admin');
 const jwtHandlers = require('../lib/jwtHandlers');
 
 module.exports = {
@@ -43,6 +44,50 @@ module.exports = {
       //const { accesstoken } = jwtHandlers.sign(findUser);
 
       return findUser;
+    } catch (error) {
+      functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+      console.log(error);
+
+      const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${req.header.user ? `uid:${req.header.user.id}` : 'req.user 없음'} ${JSON.stringify(error)}`;
+      slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
+    } finally {
+      client.release();
+    }
+  },
+  signUp: async (email, name, password) => {
+    let client;
+
+    try {
+      client = await db.connect(req);
+
+      const userFirebase = await admin
+        .auth()
+        .createUser({ email, password, name })
+        .then((user) => user)
+        .catch((e) => {
+          console.log(e);
+          return { err: true, error: e };
+        });
+
+      if (userFirebase.err) {
+        if (userFirebase.error.code === 'auth/email-already-exists') {
+          return res.status(statusCode.NOT_FOUND).json(util.fail(statusCode.NOT_FOUND, '해당 이메일을 가진 유저가 이미 있습니다.'));
+        } else if (userFirebase.error.code === 'auth/invalid-password') {
+          return res.status(statusCode.NOT_FOUND).json(util.fail(statusCode.NOT_FOUND, '비밀번호 형식이 잘못되었습니다. 패스워드는 최소 6자리의 문자열이어야 합니다.'));
+        } else {
+          return res.status(statusCode.INTERNAL_SERVER_ERROR).json(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+        }
+      }
+
+      const idFirebase = userFirebase.uid;
+
+      const user = await userDB.addUser(client, email, name, idFirebase);
+      const { accesstoken } = jwtHandlers.sign(user);
+      await userDB.setUserToken(client, user, accesstoken);
+
+      console.log(user);
+
+      return { user, accesstoken };
     } catch (error) {
       functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
       console.log(error);
