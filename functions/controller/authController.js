bb993aff25733236ab85c1c6e8c9c8cee99148f2
kanghanhee <kanghanhee59@gmail.com>
2022-05-18 23:34:05 +0900

    #133 chore: 주석 수정

--- functions/controller/authController.js
+++ functions/controller/authController.js
@@ -9,14 +9,16 @@ const { authService } = require('../service');
  *  @로그인
  *  @route POST auth/login/email
  *  @access public
- *  @err 1. 존재하지 않는 사용자일 때
- *       2. 이메일 형식이 잘못되었을 때
- *       3. 비밀번호가 일치하지 않을 때
+ *  @err 1. 필요한 값이 없을 때
+ *       2. 존재하지 않는 사용자일 때
+ *       3. 이메일 형식이 잘못되었을 때
+ *       4. 비밀번호가 일치하지 않을 때
  */
 const authEmail = async (req, res) => {
   try {
     const { email, password } = req.body;
 
+    // @err 1. 필요한 값이 없을 때
     if (!email || !password) {
       return res
         .status(statusCode.BAD_REQUEST)
@@ -25,21 +27,21 @@ const authEmail = async (req, res) => {
 
     const login = await authService.authEmail(email, password);
 
-    // @err 1. 존재하지 않는 사용자일 때
+    // @err 2. 존재하지 않는 사용자일 때
     if (login === responseMessage.NO_USER) {
       return res
         .status(statusCode.OK)
         .send(util.success(statusCode.NO_CONTENT, responseMessage.NO_USER));
     }
 
-    // @err 2. 이메일 형식이 잘못되었을 때
+    // @err 3. 이메일 형식이 잘못되었을 때
     if (login === responseMessage.WRONG_EMAIL_CONVENTION) {
       return res
         .status(statusCode.OK)
         .send(util.success(statusCode.OK, responseMessage.INVALID_EMAIL));
     }
 
-    // @err  3. 비밀번호가 일치하지 않을 때
+    // @err  4. 비밀번호가 일치하지 않을 때
     if (login === responseMessage.MISS_MATCH_PW) {
       return res
         .status(statusCode.BAD_REQUEST)
@@ -66,13 +68,15 @@ const authEmail = async (req, res) => {
  *  @회원가입
  *  @route POST /auth
  *  @access public
- *  @err 1. 이메일이 이미 존재할 때
- *       2. 패스워드 형식이 올바르지 않을 때
+ *  @err 1. 필요한 값이 없을 때
+ *       2. 이메일이 이미 존재할 때
+ *       3. 패스워드 형식이 올바르지 않을 때
  */
 const signUp = async (req, res) => {
   try {
     const { email, nickname, password } = req.body;
 
+    // @err 1. 필요한 값이 없을 때
     if (!email || !nickname || !password) {
       return res
         .status(statusCode.BAD_REQUEST)
@@ -81,14 +85,14 @@ const signUp = async (req, res) => {
 
     const newUser = await authService.signUp(email, nickname, password);
 
-    // @err 1. 이메일이 이미 존재할 때
+    // @err 2. 이메일이 이미 존재할 때
     if (newUser === responseMessage.ALREADY_EMAIL) {
       return res
         .status(statusCode.BAD_REQUEST)
         .send(util.fail(statusCode.BAD_REQUEST, responseMessage.ALREADY_EMAIL));
     }
 
-    // @err 2. 패스워드 형식이 올바르지 않을 때
+    // @err 3. 패스워드 형식이 올바르지 않을 때
     if (newUser === responseMessage.WRONG_PASSWORD_CONVENTION) {
       return res
         .status(statusCode.BAD_REQUEST)
