d53aaf626eef39c8f862285b0c95fa6de31bef3a
kanghanhee <kanghanhee59@gmail.com>
2022-05-24 23:27:14 +0900

    update: 스티커 관련 api 에러처리

--- functions/service/stickerService.js
+++ functions/service/stickerService.js
@@ -1,9 +1,12 @@
+const functions = require('firebase-functions');
 const db = require('../db/db');
 const { scheduleDB, groupDB, stickerDB } = require('../db');
+const returnType = require('../constants/returnType');
 
 module.exports = {
   getStickerBySchedule: async (user, scheduleId) => {
     let client;
+    const req = ``;
 
     try {
       client = await db.connect(req);
@@ -13,61 +16,96 @@ module.exports = {
 
       // 사용자가 보낸 스티커 리스트 조회
       const findUserLikeScheduleList = await scheduleDB.findUserLikeScheduleList(client, user.id);
-      const userLikeScheduleList = findUserLikeScheduleList.map((userLikeSchedule) => userLikeSchedule.id);
+      const userLikeScheduleList = findUserLikeScheduleList.map(
+        (userLikeSchedule) => userLikeSchedule.id,
+      );
 
       for (let i = 0; i < likeScheduleList.length; i++) {
         // 내가 보낸 스티커인지 확인
-        const isMySticker = await scheduleDB.isLikedSchedule(client, likeScheduleList[i].likeScheduleId, userLikeScheduleList);
+        const isMySticker = await scheduleDB.isLikedSchedule(
+          client,
+          likeScheduleList[i].likeScheduleId,
+          userLikeScheduleList,
+        );
         likeScheduleList[i].isMySticker = isMySticker;
       }
 
       return likeScheduleList;
     } catch (error) {
-      functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+      functions.logger.error(
+        `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`,
+        `[CONTENT] ${error}`,
+      );
       console.log(error);
-
-      const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${req.header.user ? `uid:${req.header.user.id}` : 'req.user 없음'} ${JSON.stringify(error)}`;
-      slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
     } finally {
       client.release();
     }
   },
   sendSticker: async (user, stickerId, scheduleId) => {
     let client;
+    const req = ``;
 
     try {
       client = await db.connect(req);
 
-      const findScheduleByScheduleId = await scheduleDB.findScheduleByScheduleId(client, scheduleId);
-      if (!findScheduleByScheduleId) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.OUT_OF_VALUE));
+      const findScheduleByScheduleId = await scheduleDB.findScheduleByScheduleId(
+        client,
+        scheduleId,
+      );
 
-      // 해당 스케줄 주인의 id와 유저 id로 그룹 수락 여부를 확인
+      // 존재하지 않는 스케줄일 때
+      if (!findScheduleByScheduleId) {
+        return returnType.DB_NOT_FOUND;
+      }
+
+      // 해당 스케줄 주인의 id와 유저 id로 공유 수락 여부를 확인
       const findScheduleUser = findScheduleByScheduleId.userId;
       const findSendGroup = await groupDB.findSendGroupIsOkay(client, user.id, findScheduleUser);
-      if (findSendGroup.length === 0) return res.status(statusCode.UNAUTHORIZED).send(util.fail(statusCode.UNAUTHORIZED, responseMessage.NO_AUTHENTICATED));
 
-      // 해당 스케줄의 완료 여부를 확인
-      if (findScheduleByScheduleId.isCheck === false) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NOT_CHECK));
+      // 공유 요청이 수락되지 않았을 때
+      if (findSendGroup.length === 0) {
+        return returnType.WRONG_REQUEST_VALUE;
+      }
+
+      // 해당 스케줄이 아직 완료가 되지 않았을 때
+      if (findScheduleByScheduleId.isCheck === false) {
+        return returnType.WRONG_REQUEST_VALUE;
+      }
 
       // 이미 스티커를 보냈는지 확인
-      const findLikeSchedule = await scheduleDB.findLikeScheduleBySenderId(client, scheduleId, user.id);
-      if (findLikeSchedule.length !== 0) return res.status(statusCode.CONFLICT).send(util.fail(statusCode.CONFLICT, responseMessage.ALREADY_POST_STICKER));
+      const findLikeSchedule = await scheduleDB.findLikeScheduleBySenderId(
+        client,
+        scheduleId,
+        user.id,
+      );
 
-      const addLikeSchedule = await scheduleDB.addLikeSchedule(client, scheduleId, user.id, stickerId);
+      // 이미 스티커를 전송했을 때
+      if (findLikeSchedule.length !== 0) {
+        return returnType.VALUE_ALREADY_EXIST;
+      }
+
+      // 스티커 전송하기
+      const addLikeSchedule = await scheduleDB.addLikeSchedule(
+        client,
+        scheduleId,
+        user.id,
+        stickerId,
+      );
 
       return addLikeSchedule;
     } catch (error) {
-      functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+      functions.logger.error(
+        `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`,
+        `[CONTENT] ${error}`,
+      );
       console.log(error);
-
-      const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${req.header.user ? `uid:${req.header.user.id}` : 'req.user 없음'} ${JSON.stringify(error)}`;
-      slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
     } finally {
       client.release();
     }
   },
   getAllSticker: async () => {
     let client;
+    const req = ``;
 
     try {
       client = await db.connect(req);
@@ -76,40 +114,45 @@ module.exports = {
 
       return stickerList;
     } catch (error) {
-      functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+      functions.logger.error(
+        `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`,
+        `[CONTENT] ${error}`,
+      );
       console.log(error);
-
-      const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${req.header.user ? `uid:${req.header.user.id}` : 'req.user 없음'} ${JSON.stringify(error)}`;
-      slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
     } finally {
       client.release();
     }
   },
   updateSendSticker: async (user, stickerId, likeScheduleId) => {
     let client;
+    const req = ``;
 
     try {
       client = await db.connect(req);
 
-      // 해당 스티커 정보 조회
       const findLikeScheduleById = await scheduleDB.findLikeScheduleById(client, likeScheduleId);
 
-      if (!findLikeScheduleById) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.OUT_OF_VALUE));
+      // 수정할 스티커가 없을 때
+      if (!findLikeScheduleById) {
+        return returnType.DB_NOT_FOUND;
+      }
 
       const findUser = findLikeScheduleById.senderId;
 
-      // 스티커 주인이 아닐 시 에러 반환
-      if (findUser !== user.id) return res.status(statusCode.UNAUTHORIZED).send(util.fail(statusCode.UNAUTHORIZED, responseMessage.NO_AUTHENTICATED));
+      // 스티커를 보낸 사용자가 아닐 때
+      if (findUser !== user.id) {
+        return returnType.WRONG_REQUEST_VALUE;
+      }
 
       const updateSticker = await scheduleDB.updateSticker(client, likeScheduleId, stickerId);
 
       return updateSticker;
     } catch (error) {
-      functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+      functions.logger.error(
+        `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`,
+        `[CONTENT] ${error}`,
+      );
       console.log(error);
-
-      const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${req.header.user ? `uid:${req.header.user.id}` : 'req.user 없음'} ${JSON.stringify(error)}`;
-      slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
     } finally {
       client.release();
     }
