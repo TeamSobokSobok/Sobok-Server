c62f530ef333886bc2aef81e1f08a559f949b88d
kanghanhee <kanghanhee59@gmail.com>
2022-06-25 01:29:47 +0900

    #150 feat: social login

--- functions/constants/responseMessage.js
+++ functions/constants/responseMessage.js
@@ -11,6 +11,8 @@ module.exports = {
   ALREADY_EMAIL: '이미 사용중인 이메일입니다.',
   ALREADY_NICKNAME: '이미 사용중인 닉네임입니다.',
   USEABLE_NICKNAME: '사용 가능한 닉네임입니다.',
+  ALREADY_SOCIALID: '',
+  NOT_SIGNED_UP: '',
 
   // 로그인
   LOGIN_SUCCESS: '로그인 성공',
--- functions/controller/authController.js
+++ functions/controller/authController.js
@@ -4,99 +4,33 @@ const statusCode = require('../constants/statusCode');
 const responseMessage = require('../constants/responseMessage');
 
 const { authService } = require('../service');
-
-/**
- *  @로그인
- *  @route POST /auth/login/email
- *  @access public
- *  @err 1. 필요한 값이 없을 때
- *       2. 존재하지 않는 사용자일 때
- *       3. 이메일 형식이 잘못되었을 때
- *       4. 비밀번호가 일치하지 않을 때
- */
-const authEmail = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    // @err 1. 필요한 값이 없을 때
-    if (!email || !password) {
-      return res
-        .status(statusCode.BAD_REQUEST)
-        .send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
-    }
-
-    const login = await authService.authEmail(email, password);
-
-    // @err 2. 존재하지 않는 사용자일 때
-    if (login === responseMessage.NO_USER) {
-      return res
-        .status(statusCode.OK)
-        .send(util.success(statusCode.NO_CONTENT, responseMessage.NO_USER));
-    }
-
-    // @err 3. 이메일 형식이 잘못되었을 때
-    if (login === responseMessage.WRONG_EMAIL_CONVENTION) {
-      return res
-        .status(statusCode.OK)
-        .send(util.success(statusCode.OK, responseMessage.INVALID_EMAIL));
-    }
-
-    // @err  4. 비밀번호가 일치하지 않을 때
-    if (login === responseMessage.MISS_MATCH_PW) {
-      return res
-        .status(statusCode.BAD_REQUEST)
-        .send(util.fail(statusCode.BAD_REQUEST, responseMessage.MISS_MATCH_PW));
-    }
-
-    return res
-      .status(statusCode.OK)
-      .send(util.success(statusCode.OK, responseMessage.LOGIN_SUCCESS, login));
-  } catch (error) {
-    console.log(error);
-    functions.logger.error(
-      `[EMAIL LOGIN ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`,
-      `[CONTENT] email:${email} ${error}`,
-    );
-
-    return res
-      .status(statusCode.INTERNAL_SERVER_ERROR)
-      .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
-  }
-};
+const returnType = require('../constants/returnType');
 
 /**
  *  @회원가입
- *  @route POST /auth
+ *  @route POST /auth/signup
  *  @access public
  *  @err 1. 필요한 값이 없을 때
  *       2. 이메일이 이미 존재할 때
  *       3. 패스워드 형식이 올바르지 않을 때
  */
+
 const signUp = async (req, res) => {
   try {
-    const { email, nickname, password } = req.body;
+    const { socialId, email, username } = req.body;
 
-    // @err 1. 필요한 값이 없을 때
-    if (!email || !nickname || !password) {
+    if (!socialId || !email || !username)
       return res
         .status(statusCode.BAD_REQUEST)
         .send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
-    }
 
-    const newUser = await authService.signUp(email, nickname, password);
+    const newUser = await authService.signUp(socialId, email, username);
 
-    // @err 2. 이메일이 이미 존재할 때
-    if (newUser === responseMessage.ALREADY_EMAIL) {
+    // ㅇㅣㅁㅣ ㅈㅗㄴㅈㅐㅎㅏㄴㅡㄴ ㅅㅗㅅㅕㄹ
+    if (newUser === returnType.VALUE_ALREADY_EXIST) {
       return res
         .status(statusCode.BAD_REQUEST)
-        .send(util.fail(statusCode.BAD_REQUEST, responseMessage.ALREADY_EMAIL));
-    }
-
-    // @err 3. 패스워드 형식이 올바르지 않을 때
-    if (newUser === responseMessage.WRONG_PASSWORD_CONVENTION) {
-      return res
-        .status(statusCode.BAD_REQUEST)
-        .send(util.fail(statusCode.BAD_REQUEST, responseMessage.WRONG_PASSWORD_CONVENTION));
+        .send(util.fail(statusCode.BAD_REQUEST, responseMessage.ALREADY_SOCIALID));
     }
 
     return res
@@ -115,7 +49,50 @@ const signUp = async (req, res) => {
   }
 };
 
+/**
+ *  @ㄹㅗㄱㅡㅇㅣㄴ
+ *  @route GET /auth/signin
+ *  @access public
+ *  @err 1. 필요한 값이 없을 때
+ *       2. 이메일이 이미 존재할 때
+ *       3. 패스워드 형식이 올바르지 않을 때
+ */
+
+const signIn = async (req, res) => {
+  try {
+    const { socialId } = req.body;
+
+    if (!socialId)
+      return res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+
+    const user = await authService.singIn(socialId);
+
+    // ㅇㅣㅁㅣ ㅈㅗㄴㅈㅐㅎㅏㄴㅡㄴ ㅅㅗㅅㅕㄹ
+    if (user === returnType.NON_EXISTENT_USER) {
+      return res
+        .status(statusCode.OK)
+        .send(util.success(statusCode.OK, responseMessage.NOT_SIGNED_UP, { isNew: true }));
+    }
+
+    return res
+      .status(statusCode.OK)
+      .send(util.success(statusCode.OK, responseMessage.LOGIN_SUCCESS, user));
+  } catch (error) {
+    functions.logger.error(
+      `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`,
+      `[CONTENT] ${error}`,
+    );
+    console.log(error);
+
+    res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+  }
+};
+
 module.exports = {
-  authEmail,
   signUp,
+  signIn,
 };
