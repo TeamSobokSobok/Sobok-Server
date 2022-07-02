348fee9f77f37afc0cebd43196178b19e82389c0
kanghanhee <kanghanhee59@gmail.com>
2022-07-02 11:43:34 +0900

    #166 fix: conflict

--- functions/controller/scheduleController.js
+++ functions/controller/scheduleController.js
@@ -88,176 +88,204 @@ const getMySchedule = async (req, res) => {
   }
 };
 
+/**
+ * GET /schedule/:memberId/calendar
+ * member 캘린더 조회
+ * @private
+ */
+const getMemberCalendar = async (req, res) => {
+  try {
+    const { user } = req.header;
+    const { memberId } = req.params;
+    const { date } = req.query;
+
+    if (!memberId || !date)
+      return res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+
+    const calendar = await scheduleService.getMemberCalendar(user, Number(memberId), date);
+
+    // 캘린더 공유 수락 X
+    if (calendar === returnType.WRONG_REQUEST_VALUE) {
+      return res
+        .status(statusCode.FORBIDDEN)
+        .send(util.fail(statusCode.FORBIDDEN, responseMessage.NO_AUTHENTICATED));
+    }
+
+    return res
+      .status(statusCode.OK)
+      .send(util.success(statusCode.OK, responseMessage.READ_MEMBER_CALENDAR, calendar));
+  } catch (error) {
+    functions.logger.error(
+      `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`,
+      `[CONTENT] ${error}`,
+    );
+    console.log(error);
+
+    const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${
+      req.header.user ? `uid:${req.header.user.id}` : 'req.user 없음'
+    } ${JSON.stringify(error)}`;
+    slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
+
+    return res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+  }
+};
+
+/**
+ * GET /schedule/:memberId/detail
+ * member schedule 조회
+ * @private
+ */
+const getMemberSchedule = async (req, res) => {
+  try {
+    const { user } = req.header;
+    const { memberId } = req.params;
+    let { date } = req.query;
+
+    if (!memberId || !date)
+      return res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+
+    const scheduleList = await scheduleService.getMemeberSchedule(user, Number(memberId), date);
+    // 캘린더 공유 수락 X
+    if (scheduleList === returnType.WRONG_REQUEST_VALUE) {
+      return res
+        .status(statusCode.FORBIDDEN)
+        .send(util.fail(statusCode.FORBIDDEN, responseMessage.NO_AUTHENTICATED));
+    }
+
+    return res
+      .status(statusCode.OK)
+      .send(util.success(statusCode.OK, responseMessage.READ_MEMBER_SCHEDULE, scheduleList));
+  } catch (error) {
+    functions.logger.error(
+      `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`,
+      `[CONTENT] ${error}`,
+    );
+    console.log(error);
+
+    const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${
+      req.header.user ? `uid:${req.header.user.id}` : 'req.user 없음'
+    } ${JSON.stringify(error)}`;
+    slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
+
+    return res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+  }
+};
+
+/**
+ * PUT /schedule/check/:scheduleId
+ * update schedule
+ * @private
+ */
+const checkSchedule = async (req, res) => {
+  try {
+    const { user } = req.header;
+    const { scheduleId } = req.params;
+
+    if (!scheduleId) {
+      return res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+    }
+
+    const result = await scheduleService.checkSchedule(user, Number(scheduleId));
+
+    if (result === returnType.DB_NOT_FOUND) {
+      return res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, responseMessage.OUT_OF_VALUE));
+    }
+
+    if (result === returnType.WRONG_REQUEST_VALUE) {
+      return res
+        .status(statusCode.FORBIDDEN)
+        .send(util.fail(statusCode.FORBIDDEN, responseMessage.NO_AUTHENTICATED));
+    }
+
+    return res
+      .status(statusCode.OK)
+      .send(util.success(statusCode.OK, responseMessage.UPDATE_SCHEDULE_CHECK, result));
+  } catch (error) {
+    functions.logger.error(
+      `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`,
+      `[CONTENT] ${error}`,
+    );
+    console.log(error);
+
+    const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${
+      req.header.user ? `uid:${req.header.user.id}` : 'req.user 없음'
+    } ${JSON.stringify(error)}`;
+    slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
+
+    return res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+  }
+};
+
+/**
+ * PUT /schedule/uncheck/:scheduleId
+ * update schedule
+ * @private
+ */
+const unCheckSchedule = async (req, res) => {
+  try {
+    const { user } = req.header;
+    const { scheduleId } = req.params;
+
+    if (!scheduleId) {
+      return res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+    }
+
+    const result = await scheduleService.unCheckSchedule(user, Number(scheduleId));
+
+    if (result === returnType.DB_NOT_FOUND) {
+      return res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, responseMessage.OUT_OF_VALUE));
+    }
+
+    if (result === returnType.WRONG_REQUEST_VALUE) {
+      return res
+        .status(statusCode.FORBIDDEN)
+        .send(util.fail(statusCode.FORBIDDEN, responseMessage.NO_AUTHENTICATED));
+    }
+
+    return res
+      .status(statusCode.OK)
+      .send(util.success(statusCode.OK, responseMessage.UPDATE_SCHEDULE_UNCHECK, result));
+  } catch (error) {
+    functions.logger.error(
+      `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`,
+      `[CONTENT] ${error}`,
+    );
+    console.log(error);
+
+    const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${
+      req.header.user ? `uid:${req.header.user.id}` : 'req.user 없음'
+    } ${JSON.stringify(error)}`;
+    slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
+
+    return res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+  }
+};
+
 module.exports = {
   getMyCalendar,
   getMySchedule,
-  getMemberCalendar: async (req, res) => {
-    try {
-      const { user } = req.header;
-      const { memberId } = req.params;
-      const { date } = req.query;
-
-      if (!memberId || !date)
-        return res
-          .status(statusCode.BAD_REQUEST)
-          .send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
-
-      const calendar = await scheduleService.getMemberCalendar(user, Number(memberId), date);
-
-      // 캘린더 공유 수락 X
-      if (calendar === returnType.WRONG_REQUEST_VALUE) {
-        return res
-          .status(statusCode.FORBIDDEN)
-          .send(util.fail(statusCode.FORBIDDEN, responseMessage.NO_AUTHENTICATED));
-      }
-
-      return res
-        .status(statusCode.OK)
-        .send(util.success(statusCode.OK, responseMessage.READ_MEMBER_CALENDAR, calendar));
-    } catch (error) {
-      functions.logger.error(
-        `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`,
-        `[CONTENT] ${error}`,
-      );
-      console.log(error);
-
-      const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${
-        req.header.user ? `uid:${req.header.user.id}` : 'req.user 없음'
-      } ${JSON.stringify(error)}`;
-      slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
-
-      return res
-        .status(statusCode.INTERNAL_SERVER_ERROR)
-        .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
-    }
-  },
-  getMemberSchedule: async (req, res) => {
-    try {
-      const { user } = req.header;
-      const { memberId } = req.params;
-      let { date } = req.query;
-
-      if (!memberId || !date)
-        return res
-          .status(statusCode.BAD_REQUEST)
-          .send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
-
-      const scheduleList = await scheduleService.getMemeberSchedule(user, Number(memberId), date);
-      // 캘린더 공유 수락 X
-      if (scheduleList === returnType.WRONG_REQUEST_VALUE) {
-        return res
-          .status(statusCode.FORBIDDEN)
-          .send(util.fail(statusCode.FORBIDDEN, responseMessage.NO_AUTHENTICATED));
-      }
-
-      return res
-        .status(statusCode.OK)
-        .send(util.success(statusCode.OK, responseMessage.READ_MEMBER_SCHEDULE, scheduleList));
-    } catch (error) {
-      functions.logger.error(
-        `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`,
-        `[CONTENT] ${error}`,
-      );
-      console.log(error);
-
-      const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${
-        req.header.user ? `uid:${req.header.user.id}` : 'req.user 없음'
-      } ${JSON.stringify(error)}`;
-      slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
-
-      return res
-        .status(statusCode.INTERNAL_SERVER_ERROR)
-        .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
-    }
-  },
-  checkSchedule: async (req, res) => {
-    try {
-      const { user } = req.header;
-      const { scheduleId } = req.params;
-
-      if (!scheduleId) {
-        return res
-          .status(statusCode.BAD_REQUEST)
-          .send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
-      }
-
-      const result = await scheduleService.checkSchedule(user, Number(scheduleId));
-
-      if (result === returnType.DB_NOT_FOUND) {
-        return res
-          .status(statusCode.BAD_REQUEST)
-          .send(util.fail(statusCode.BAD_REQUEST, responseMessage.OUT_OF_VALUE));
-      }
-
-      if (result === returnType.WRONG_REQUEST_VALUE) {
-        return res
-          .status(statusCode.FORBIDDEN)
-          .send(util.fail(statusCode.FORBIDDEN, responseMessage.NO_AUTHENTICATED));
-      }
-
-      return res
-        .status(statusCode.OK)
-        .send(util.success(statusCode.OK, responseMessage.UPDATE_SCHEDULE_CHECK, result));
-    } catch (error) {
-      functions.logger.error(
-        `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`,
-        `[CONTENT] ${error}`,
-      );
-      console.log(error);
-
-      const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${
-        req.header.user ? `uid:${req.header.user.id}` : 'req.user 없음'
-      } ${JSON.stringify(error)}`;
-      slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
-
-      return res
-        .status(statusCode.INTERNAL_SERVER_ERROR)
-        .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
-    }
-  },
-  unCheckSchedule: async (req, res) => {
-    try {
-      const { user } = req.header;
-      const { scheduleId } = req.params;
-
-      if (!scheduleId) {
-        return res
-          .status(statusCode.BAD_REQUEST)
-          .send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
-      }
-
-      const result = await scheduleService.unCheckSchedule(user, Number(scheduleId));
-
-      if (result === returnType.DB_NOT_FOUND) {
-        return res
-          .status(statusCode.BAD_REQUEST)
-          .send(util.fail(statusCode.BAD_REQUEST, responseMessage.OUT_OF_VALUE));
-      }
-
-      if (result === returnType.WRONG_REQUEST_VALUE) {
-        return res
-          .status(statusCode.FORBIDDEN)
-          .send(util.fail(statusCode.FORBIDDEN, responseMessage.NO_AUTHENTICATED));
-      }
-
-      return res
-        .status(statusCode.OK)
-        .send(util.success(statusCode.OK, responseMessage.UPDATE_SCHEDULE_UNCHECK, result));
-    } catch (error) {
-      functions.logger.error(
-        `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`,
-        `[CONTENT] ${error}`,
-      );
-      console.log(error);
-
-      const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${
-        req.header.user ? `uid:${req.header.user.id}` : 'req.user 없음'
-      } ${JSON.stringify(error)}`;
-      slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
-
-      return res
-        .status(statusCode.INTERNAL_SERVER_ERROR)
-        .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
-    }
-  },
+  getMemberCalendar,
+  getMemberSchedule,
+  checkSchedule,
+  unCheckSchedule,
 };
--- functions/service/scheduleService.js
+++ functions/service/scheduleService.js
@@ -6,181 +6,97 @@ const { scheduleDB, groupDB, stickerDB } = require('../db');
 const db = require('../db/db');
 const util = require('../lib/util');
 
-/**
- * 내 캘린더 전체 조회
- * @param date - 현재 날짜
- * @param userId - 해당 유저 아이디
- */
-const getMyCalendar = async (date, userId) => {
-  let client;
-  const log = `scheduleDB.getMyCalendar | date = ${date}`;
-
-  try {
-    client = await db.connect(log);
-
-    const startMonth = dayjs(date).startOf('month').format('YYYY-MM-DD');
-    const endMonth = dayjs(date).endOf('month').format('YYYY-MM-DD');
-
-    const myCalender = await scheduleDB.getMyCalendar(client, userId, startMonth, endMonth);
-    myCalender.forEach((data) => {
-      if (data.scheduleCount === data.isCheckCount) {
-        data.isComplete = 'done';
-      } else if (data.scheduleCount > data.isCheckCount && Number(data.isCheckCount) > 0) {
-        data.isComplete = 'doing';
-      } else if (Number(data.isCheckCount) === 0) {
-        data.isComplete = 'none';
-      }
-    });
-
-    return util.success(statusCode.OK, responseMessage.READ_MY_CALENDAR, myCalender);
-  } catch (error) {
-    console.error('getMyCalendar error 발생: ' + error);
-  } finally {
-    client.release();
-  }
-};
-
-/**
- * 해당 일정 복약 스케줄 조회
- * @param userId - 유저 아이디
- * @param date - 조회를 원하는 날짜
- */
-const getMySchedule = async (userId, date) => {
-  let client;
-  const log = `scheduleDB.getMySchedule | userId = ${userId}, date = ${date}`;
-
-  try {
-    client = await db.connect(db);
-
-    const myScheduleTime = await scheduleDB.getMyScheduleTime(client, userId, date);
-    for (let data = 0; data < myScheduleTime.length; data++) {
-      let schedule = await scheduleDB.getMySchedule(
-        client,
-        userId,
-        date,
-        myScheduleTime[data].scheduleTime,
-      );
-
-      for (let scheduleInfo = 0; scheduleInfo < schedule.length; scheduleInfo++) {
-        let stickerCount = {
-          1: 0,
-          2: 0,
-          3: 0,
-          4: 0,
-          5: 0,
-        };
-        let stickerList = await stickerDB.findStickerListById(
-          client,
-          userId,
-          schedule[scheduleInfo].scheduleId,
-        );
-
-        for (let sticker = 0; sticker < stickerList.length; sticker++) {
-          if (stickerList[sticker].stickerId === 1) {
-            stickerCount[1]++;
-          } else if (stickerList[sticker].stickerId === 2) {
-            stickerCount[2]++;
-          } else if (stickerList[sticker].stickerId === 3) {
-            stickerCount[3]++;
-          } else if (stickerList[sticker].stickerId === 4) {
-            stickerCount[4]++;
-          } else if (stickerList[sticker].stickerId === 5) {
-            stickerCount[5]++;
-          }
-        }
-        schedule[scheduleInfo].stickerId = stickerCount;
-        schedule[scheduleInfo].stickerTotalCount = stickerList.length;
-      }
-      myScheduleTime[data].scheduleList = schedule;
-    }
-
-    return util.success(statusCode.OK, responseMessage.READ_MY_SCHEDULE, myScheduleTime);
-  } catch (error) {
-    console.log('getMySchedule error 발생: ' + error);
-  } finally {
-    client.release();
-  }
-};
-
 module.exports = {
-  getMyCalendar: async (user, date) => {
+  /**
+   * 내 캘린더 전체 조회
+   * @param date - 현재 날짜
+   * @param userId - 해당 유저 아이디
+   */
+  getMyCalendar: async (date, userId) => {
     let client;
-    const req = ``;
+    const log = `scheduleDB.getMyCalendar | date = ${date}`;
 
     try {
-      client = await db.connect(req);
+      client = await db.connect(log);
 
-      // Request로 받은 날짜를 한달 기준으로 startDate, endDate 선언
-      const startDate = dayjs(date).startOf('month').format('YYYY-MM-DD');
-      const endDate = dayjs(date).endOf('month').format('YYYY-MM-DD');
+      const startMonth = dayjs(date).startOf('month').format('YYYY-MM-DD');
+      const endMonth = dayjs(date).endOf('month').format('YYYY-MM-DD');
 
-      // 해당 유저의 아이디로 schedule 정보 조회
-      const myCalender = await scheduleDB.findCalendarByMemberId(
-        client,
-        user.id,
-        startDate,
-        endDate,
-      );
+      const myCalender = await scheduleDB.getMyCalendar(client, userId, startMonth, endMonth);
+      myCalender.forEach((data) => {
+        if (data.scheduleCount === data.isCheckCount) {
+          data.isComplete = 'done';
+        } else if (data.scheduleCount > data.isCheckCount && Number(data.isCheckCount) > 0) {
+          data.isComplete = 'doing';
+        } else if (Number(data.isCheckCount) === 0) {
+          data.isComplete = 'none';
+        }
+      });
 
-      // 약 스케줄 개수와 체크 완료된 스케줄 개수 비교
-      for (let i = 0; i < myCalender.length; i++) {
-        const scheduleCount = myCalender[i].scheduleCount;
-        const isCheckCount = myCalender[i].isCheckCount;
-
-        if (scheduleCount === isCheckCount) {
-          myCalender[i].isComplete = 'done';
-        } else if (isCheckCount === '0') {
-          myCalender[i].isComplete = 'none';
-        } else myCalender[i].isComplete = 'doing';
-      }
-
-      return myCalender;
+      return util.success(statusCode.OK, responseMessage.READ_MY_CALENDAR, myCalender);
     } catch (error) {
-      console.log(error);
+      console.error('getMyCalendar error 발생: ' + error);
     } finally {
       client.release();
     }
   },
-  getMySchedule: async (user, date) => {
+  /**
+   * 해당 일정 복약 스케줄 조회
+   * @param userId - 유저 아이디
+   * @param date - 조회를 원하는 날짜
+   */
+  getMySchedule: async (userId, date) => {
     let client;
-    const req = ``;
+    const log = `scheduleDB.getMySchedule | userId = ${userId}, date = ${date}`;
 
     try {
-      client = await db.connect(req);
+      client = await db.connect(db);
 
-      date = dayjs(date).format('YYYY-MM-DD');
-
-      // 해당 멤버의 스케줄 날짜에 대한 시간 정보 불러오기
-      let findmemberScheduleTime = await scheduleDB.findScheduleTime(client, user.id, date);
-
-      for (let i = 0; i < findmemberScheduleTime.length; i++) {
-        // 시간에 대한 스케줄 리스트 불러오기
-        let scheduleList = await scheduleDB.findScheduleByMemberId(
+      const myScheduleTime = await scheduleDB.getMyScheduleTime(client, userId, date);
+      for (let data = 0; data < myScheduleTime.length; data++) {
+        let schedule = await scheduleDB.getMySchedule(
           client,
-          user.id,
+          userId,
           date,
-          findmemberScheduleTime[i].scheduleTime,
+          myScheduleTime[data].scheduleTime,
         );
-        findmemberScheduleTime[i].scheduleList = scheduleList;
 
-        for (let v = 0; v < scheduleList.length; v++) {
-          // 스케줄id로 스티커 4개 불러오기
-          let scheduleId = scheduleList[v].scheduleId;
-          let stickerList = await scheduleDB.findMyLikeScheduleByScheduleId(client, scheduleId);
-          scheduleList[v].stickerId = stickerList;
-
-          // 약 스케줄에 대한 전체 스티커 리스트 조회
-          let stickerTotalCount = await scheduleDB.findAllLikeScheduleByScheduleId(
+        for (let scheduleInfo = 0; scheduleInfo < schedule.length; scheduleInfo++) {
+          let stickerCount = {
+            1: 0,
+            2: 0,
+            3: 0,
+            4: 0,
+            5: 0,
+          };
+          let stickerList = await stickerDB.findStickerListById(
             client,
-            scheduleId,
+            userId,
+            schedule[scheduleInfo].scheduleId,
           );
-          scheduleList[v].stickerTotalCount = stickerTotalCount.length;
+
+          for (let sticker = 0; sticker < stickerList.length; sticker++) {
+            if (stickerList[sticker].stickerId === 1) {
+              stickerCount[1]++;
+            } else if (stickerList[sticker].stickerId === 2) {
+              stickerCount[2]++;
+            } else if (stickerList[sticker].stickerId === 3) {
+              stickerCount[3]++;
+            } else if (stickerList[sticker].stickerId === 4) {
+              stickerCount[4]++;
+            } else if (stickerList[sticker].stickerId === 5) {
+              stickerCount[5]++;
+            }
+          }
+          schedule[scheduleInfo].stickerId = stickerCount;
+          schedule[scheduleInfo].stickerTotalCount = stickerList.length;
         }
+        myScheduleTime[data].scheduleList = schedule;
       }
 
-      return findmemberScheduleTime;
+      return util.success(statusCode.OK, responseMessage.READ_MY_SCHEDULE, myScheduleTime);
     } catch (error) {
-      console.log(error);
+      console.log('getMySchedule error 발생: ' + error);
     } finally {
       client.release();
     }
